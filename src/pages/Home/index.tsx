9267c26f3981711d703c297f52d4b878b1d21bbd
iasmindanyelly <iasmindanyelly@gmail.com>
2024-10-26 19:05:07 -0300

    alterando Banner 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,14 @@ export const Home = () => {
   return (
   
     <div 
-      className="flex bg-cover bg-fixed bg-parallax h-[100vh]  home items-center   object-cover   flex-col   bg-left-top"     >
+      className="flex bg-cover bg-fixed 
+      home
+      items-center 
+      object-cover 
+      flex-col 
+      bg-left-top" 
+      style={{ backgroundImage: `url("src/assets/fotoBannerFernando.jpg")` }}
+    >
       <Header/>
       <Banner/>
     </div>
