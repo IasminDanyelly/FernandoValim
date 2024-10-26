d387f692d76c8cb9c80c0f318c753999efb63f03
iasmindanyelly <iasmindanyelly@gmail.com>
2024-10-26 18:35:14 -0300

    integrações

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,17 +5,19 @@ import { Header } from "../../components/Header";
 export const Home = () => {
   return (
   
-    <div className="flex bg-parallax bg-cover bg-fixed 
-     h-[60vh]
-     sm:h-[100vh] 
-     md:h-[60vh] 
-     lg:h-[80vh] 
-     xl:h-[100vh] 
-     items-center 
-     object-cover 
-     flex-col 
-     bg-left-top   
-     ">
+    <div 
+    className="flex bg-cover bg-fixed 
+      h-[60vh]
+      sm:h-[100vh] 
+      md:h-[60vh] 
+      lg:h-[80vh] 
+      xl:h-[100vh] 
+      items-center 
+      object-cover 
+      flex-col 
+      bg-left-top" 
+      style={{ backgroundImage: `url("src/assets/fotoBannerFernando.jpg")` }}
+    >
       <Header/>
       <Banner/>
     </div>
