6ee3c68c046397f0b9718a3982726424dd2433b9
iasmindanyelly <iasmindanyelly@gmail.com>
2024-10-29 07:18:22 -0300

    alteração no banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import { Banner } from "../../components/Banner";
 import { Header } from "../../components/Header";
-
+import BgImage from '../../assets/fotoFernando.jpg'
 
 export const Home = () => {
   return (
     <div 
       className="flex bg-cover bg-fixed flex-col  items-center object-cover home h-[100vh]" 
-      style={{ backgroundImage: `url("https://instagram.fccm11-1.fna.fbcdn.net/v/t51.293…iBWnc3TbDgiAi_AWW7iEvw&oe=672697BB&_nc_sid=7a9f4b")` }}
+      style={{ backgroundImage: `url(${BgImage})` }}
     >
       <Header/>
       <Banner/>
